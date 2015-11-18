7e583aa98ab155a9a09041467b651d76040c6418
Christian Eisendle <christian@eisendle.net>
2015-11-18 21:25:13 +0100

    Make the database name configurable

--- capture_and_update_db.sh
+++ capture_and_update_db.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+BW_DATABASE_NAME=bandwidth.rrd
 . ./bandwidth.cfg
 val=`iperf -c ${BW_IPERF_SERVER} -r -t 5 -y c | awk -F "," 'BEGIN {printf("N:"); cnt = 0;} {if (cnt++ == 1) printf(":"); printf("%d", $9)}'`
-/usr/bin/rrdtool update bandwidth.rrd $val
+/usr/bin/rrdtool update $BW_DATABASE_NAME $val
--- capture_and_update_db_iperf3.sh
+++ capture_and_update_db_iperf3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+BW_DATABASE_NAME=bandwidth.rrd
 . ./bandwidth.cfg
 us=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
 ds=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J -R | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
 if [ $us -eq 0 ]; then us="U"; fi
 if [ $ds -eq 0 ]; then ds="U"; fi
-/usr/bin/rrdtool update bandwidth.rrd "N:$us:$ds"
+/usr/bin/rrdtool update $BW_DATABASE_NAME "N:$us:$ds"
