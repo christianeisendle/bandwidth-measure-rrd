ecbf413ddabeff461b12e133aebf38302585c09c
Christian Eisendle <christian@eisendle.net>
2015-11-18 21:31:55 +0100

    set default database

--- capture_and_update_db_iperf3.sh
+++ capture_and_update_db_iperf3.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-BW_DATABASE_NAME=bandwidth.rrd
 . ./bandwidth.cfg
+if [ -z $BW_DATABASE_NAME ]; then 
+  BW_DATABASE_NAME=bandwidth.rrd
+fi
 us=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
 ds=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J -R | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
 if [ $us -eq 0 ]; then us="U"; fi
