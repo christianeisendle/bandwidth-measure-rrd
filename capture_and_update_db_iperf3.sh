c25a3d633af789402142626f53af7667e23e0b50
Christian Eisendle <christian@eisendle.net>
2015-11-18 20:52:55 +0100

    Add support for iperf3. Requires jq.

---
+++ capture_and_update_db_iperf3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+. ./bandwidth.cfg
+us=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
+ds=`iperf3 -c ${BW_IPERF_SERVER} -t 5 -J -R | jq '.end.sum_received.bytes' | awk '{ printf("%d", ($1 * 8) / 5)}'`
+if [ $us -eq 0 ]; then us="U"; fi
+if [ $ds -eq 0 ]; then ds="U"; fi
+/usr/bin/rrdtool update bandwidth.rrd "N:$us:$ds"
