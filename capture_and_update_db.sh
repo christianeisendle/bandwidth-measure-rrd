ee7c3c6d2fdf2ef076c5370636190add66d31fdd
Christian Eisendle <christian@eisendle.net>
2015-11-24 15:33:29 +0100

    Remove capture_and_update_db.sh, in fact iperf v2 support. Closes #6.

--- capture_and_update_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. ./bandwidth.cfg
-if [ -z $BW_DATABASE_NAME ]; then 
-  BW_DATABASE_NAME=bandwidth.rrd
-fi
-val=`iperf -c ${BW_IPERF_SERVER} -r -t 5 -y c | awk -F "," 'BEGIN {printf("N:"); cnt = 0;} {if (cnt++ == 1) printf(":"); printf("%d", $9)}'`
-/usr/bin/rrdtool update $BW_DATABASE_NAME $val
