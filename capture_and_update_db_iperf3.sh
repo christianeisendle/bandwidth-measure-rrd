8ab40b91fdefbfd6e692d08e689a766cbffe1784
Developer <developer@eisendle.net>
2015-12-01 21:20:24 +0100

    Make number of seconds to omit configurable
    The number of seconds to omit when starting the test is fixed (2 seconds).
    This fix make it configurable by introducing a dedicated variable BW_SECS_TO_OMIT.

--- capture_and_update_db_iperf3.sh
+++ capture_and_update_db_iperf3.sh
@@ -6,8 +6,11 @@ fi
 if [ -z $BW_PERIOD_IN_SEC ]; then 
   BW_PERIOD_IN_SEC=5
 fi
-us=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -O2 | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
-ds=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -R -O2 | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
+if [ -z $BW_SECS_TO_OMIT ]; then 
+  BW_SECS_TO_OMIT=2
+fi
+us=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -O${BW_SECS_TO_OMIT} | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
+ds=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -R -O${BW_SECS_TO_OMIT} | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
 if [ $us -eq 0 ]; then us="U"; fi
 if [ $ds -eq 0 ]; then ds="U"; fi
 ${TEST} /usr/bin/rrdtool update $BW_DATABASE_NAME "N:$us:$ds"
