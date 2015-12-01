bf354b11b951517f0b7907b214da251734bd1518
Developer <developer@eisendle.net>
2015-12-01 21:54:20 +0100

    Make sure BW_PERIOD_IN_SEC > BW_SECS_TO_OMIT

--- capture_and_update_db_iperf3.sh
+++ capture_and_update_db_iperf3.sh
@@ -9,6 +9,10 @@ fi
 if [ -z $BW_SECS_TO_OMIT ]; then 
   BW_SECS_TO_OMIT=2
 fi
+if [ ${BW_PERIOD_IN_SEC} -lt 3 ]; then
+  BW_SECS_TO_OMIT=0
+fi
+    
 us=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -O${BW_SECS_TO_OMIT} | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
 ds=`iperf3 -c ${BW_IPERF_SERVER} -t ${BW_PERIOD_IN_SEC} -J -R -O${BW_SECS_TO_OMIT} | jq '.end.sum_received.bytes' | awk -v period=${BW_PERIOD_IN_SEC} '{ printf("%ld", ($0 * 8) / period)}'`
 if [ $us -eq 0 ]; then us="U"; fi
